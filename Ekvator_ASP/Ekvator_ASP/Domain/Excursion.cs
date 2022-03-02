b41432dafd92cf423ac56cd643cbdb56b1159b79
zahariewa <v.zaharieva2017@pmg-pernik.com>
2022-03-02 13:14:51 +0200

    Add Excursion model

---
+++ Ekvator_ASP/Ekvator_ASP/Domain/Excursion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ekvator_ASP.Domain
+{
+    public class Excursion
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Transport { get; set; }
+        public decimal Price { get; set; }
+        public int Capacity { get; set; }
+    }
+}
