0229421f72d218e22a37b1b1bed3cebdcf250414
zahariewa <v.zaharieva2017@pmg-pernik.com>
2022-04-20 08:44:21 +0300

    Add Menu

---
+++ Ekvator_ASP/Ekvator_ASP/Controllers/ExcursionsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Ekvator_ASP.Data;
+using Ekvator_ASP.Entities;
+
+namespace Ekvator_ASP.Controllers
+{
+    public class ExcursionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExcursionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Excursions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Excursions.ToListAsync());
+        }
+        public async Task<IActionResult> Bul()
+        {
+            return View(await _context.Excursions.Where(x=>x.Destination=="България").ToListAsync());
+        }
+
+        // GET: Excursions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var excursion = await _context.Excursions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (excursion == null)
+            {
+                return NotFound();
+            }
+
+            return View(excursion);
+        }
+
+        // GET: Excursions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Excursions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Destination,Description,StartDate,EndDate,Transport,Price,Capacity")] Excursion excursion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(excursion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(excursion);
+        }
+
+        // GET: Excursions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var excursion = await _context.Excursions.FindAsync(id);
+            if (excursion == null)
+            {
+                return NotFound();
+            }
+            return View(excursion);
+        }
+
+        // POST: Excursions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Destination,Description,StartDate,EndDate,Transport,Price,Capacity")] Excursion excursion)
+        {
+            if (id != excursion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(excursion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExcursionExists(excursion.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(excursion);
+        }
+
+        // GET: Excursions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var excursion = await _context.Excursions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (excursion == null)
+            {
+                return NotFound();
+            }
+
+            return View(excursion);
+        }
+
+        // POST: Excursions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var excursion = await _context.Excursions.FindAsync(id);
+            _context.Excursions.Remove(excursion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExcursionExists(int id)
+        {
+            return _context.Excursions.Any(e => e.Id == id);
+        }
+    }
+}
